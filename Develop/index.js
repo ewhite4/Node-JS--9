d0d6b19f64f3e44572ebb9379117e33a7f1758e6
ewhite <ew117mc@yahoo.com>
2022-05-16 12:40:54 -0700

    index.js changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');\
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            }   else {
+                    console.log('Error, please enter title!');
+                    return false;
+            }        
+        }
+    },
+    {
+        type: 'input',
+        name: 'GitHubUserName',
+        message: 'What is your GitHub username?',
+        validate: gitHubInput => {
+            if (gitHubInput) {
+                return true;
+            } else {
+                console.log("Error, please enter GitHub username!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Error, please enter email!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'what',
+        message: 'What is your project?',
+        validate: whatInput => {
+            if (whatInput) {
+                return true;
+            } else {
+                console.log("Please enter project!");
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
