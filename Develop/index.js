c9a181b7e13477b6775f6d1d719bedbdc12a5733
ewhite <ew117mc@yahoo.com>
2022-07-04 09:34:51 -0700

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,84 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');\
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { writeReadMeFile } = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project name?',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            }   else {
-                    console.log('Error, please enter title!');
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your github username",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your github.");
                     return false;
-            }        
-        }
-    },
-    {
-        type: 'input',
-        name: 'GitHubUserName',
-        message: 'What is your GitHub username?',
-        validate: gitHubInput => {
-            if (gitHubInput) {
-                return true;
-            } else {
-                console.log("Error, please enter GitHub username!");
-                return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Error, please enter email!');
-                return false;
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your E-mail Address (Required)",
+            validate: emailInput => {
+                if(emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your E-mail Address!");
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'what',
-        message: 'What is your project?',
-        validate: whatInput => {
-            if (whatInput) {
-                return true;
-            } else {
-                console.log("Please enter project!");
-                return false;
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "Enter the title of your project (Required)",
+            validate: titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the title of your project!");
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: ''
-    }
-];
+        },
+        {
+            type: "input",
+            name: "desc",
+            message: "Provide a description of the project"
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "Please provide installation instructions"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide usage information."
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please choose a license.",
+            choices: ["MIT","Apache", "Boost", "(none)"]
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Please provide contributors and guidelines"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please provide a list of instructions"
+        },
+    ])
+    .then(response => {
+        console.log(responses);
+        writeReadMeFile(responses);
+    });
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,73 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require("fs/promises");
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch (license){
+      case "MIT":
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT]";
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+        case "Apache":
+          return  "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
 
-// TODO: Create a function to generate markdown for README
+        case "Boost":
+          return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+
+        case "(None)":
+          return "";
+    };
+  };
+
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const { title, desc, install, usage, contribution, tests, github, email, license } = data;
+  const badge = renderLicenseBadge(license);
 
+  return `# ${data.title}
+  ${badge}
+
+  ## Description
+
+  ${desc}
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
+  ## Installation
+
+  ${install}
+
+  ## Usage
+
+  ${usage}
+
+
+## License
+
+${license}
+
+## Contributing
+
+${contribution}
+
+## Tests
+
+${tests}
+
+## Questions
+
+Any questions or comments please contact me here.
+* Github: [${github}](https://github.com/${github})
+* Email Address: [${email}](mailto:${email})
 `;
 }
 
-module.exports = generateMarkdown;
+function writeReadMeFile (responses) {
+  const fileContent = generateMarkdown(responses);
+  return fs.writeFile('./dist/Readme.md', fileContent);
+};
+
+module.exports = {writeReadMeFile};
